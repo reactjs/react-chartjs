8ee0249c3ca4739de6fb19529d15ad3b63230932
danmolitor <danmolitor91@gmail.com>
2016-03-19 18:30:59 -0700

    (feat) Chart.js 2.0-dev compatibility

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 module.exports = {
   Bar: require('./lib/bar'),
+  Bubble: require('./lib/bubble'),
   Doughnut: require('./lib/doughnut'),
   Line: require('./lib/line'),
   Pie: require('./lib/pie'),
   PolarArea: require('./lib/polar-area'),
   Radar: require('./lib/radar'),
+  Scatter: require('./lib/scatter'),
   createClass: require('./lib/core').createClass
 };
