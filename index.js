beca92efc5fcd16f731c692cb92c210e49e94cd8
Suraj Sapkota <ssapkota@gmail.com>
2017-02-25 13:53:17 +0545

    Upgraded to support chart.js v2.5.0+
    * Removed support for chart.js 1x
    * Added 'Bubble' and 'HorizontalBar' support

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const createClass = require('./lib/core').createClass;
+
 module.exports = {
-  Bar: require('./lib/bar'),
-  Doughnut: require('./lib/doughnut'),
-  Line: require('./lib/line'),
-  Pie: require('./lib/pie'),
-  PolarArea: require('./lib/polar-area'),
-  Radar: require('./lib/radar'),
-  createClass: require('./lib/core').createClass
+  Doughnut: createClass('doughnut', []),
+  Pie: createClass('pie', []),
+  Line: createClass('line', []),
+  Bar: createClass('bar', []),
+  Radar: createClass('radar', []),
+  PolarArea: createClass('polarArea', []),
+  Bubble: createClass('bubble', []),
+  HorizontalBar: createClass('horizontalBar', []),
+  createClass: createClass
 };
